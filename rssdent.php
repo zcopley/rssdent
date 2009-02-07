1528f230abee479a56d14af529e4b174c5fd1768
Zach Copley <zach@controlyourself.ca>
2009-02-07 15:00:28 -0800

    Adding initial version of rssdent, a sample cron job, and feed parser lib

---
+++ rssdent.php
@@ -0,0 +1,185 @@
+<?php
+//
+//	rssdent.php
+//
+//	h@x0red By Zach Copley (http://identi.ca/zach), 2008-07-29.
+//	Give this script an RSS feed to suck on, and it will spit the latest
+//	items in the feed out as dents on your indenti.ca/Laconica account.
+//	Uses ur1.ca to shorten urls.  Requires PHP 5 and SimplePie 1.1.1.
+//
+require_once('simplepie.inc');
+
+$NICKNAME = 'MYNICKNAME';
+$PASSWORD = 'MYPASSWORD';
+$FEEDURL = 'http://wendolonia.com/blog/feed/';
+$LACONICA_BASE = 'http://identi.ca';
+$CURRENT_NOTICE_URL = "$LACONICA_BASE/api/users/show/$NICKNAME.json";
+$LACONICA_UPDATE_URL = "$LACONICA_BASE/api/statuses/update.xml";
+
+// Add some hashtags to items
+$HASHTAGS = array(
+	'(mars|martian)' => '#mars',
+	'ghost' => '#ghosts',
+	'chupa' => '#chupacabra',
+	'bigfoot' => '#bigfoot',
+	'chemtrail' => '#chemtrails',
+	'robot' => '#robot'
+	);
+
+date_default_timezone_set('UTC'); # Make sure this is same TZ as your feed!
+
+$current_notice_date = strtotime(get_current_notice_date($CURRENT_NOTICE_URL));
+
+$feed = new SimplePie();
+$feed->set_feed_url($FEEDURL);
+$feed->enable_cache(false);
+$feed->enable_order_by_date(true);
+$feed->init();
+
+$notices = array();
+
+foreach ($feed->get_items() as $item) {
+	$notice = process_item($item);
+	if ($notice) {
+		array_unshift($notices, $notice);
+	}
+}
+
+if ($notices) {
+	array_map(send_notice, $notices);
+} else {
+	print "No new items to send.\n";
+}
+
+exit();
+
+
+function setup_curl() {
+	$ch = curl_init();
+	curl_setopt($ch, CURLOPT_USERAGENT, "rssdent");
+	curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 2);
+	curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+	curl_setopt($ch, CURLOPT_FAILONERROR, 1);
+	curl_setopt($ch, CURLOPT_TIMEOUT, 10);
+
+	return $ch;
+}
+
+function get_current_notice_date($url) {
+
+	$ch = setup_curl();
+	curl_setopt($ch, CURLOPT_URL, $url);
+
+	$buffer = curl_exec($ch);
+
+	if (!$buffer) {
+		printf("cURL error: %s\n", curl_error($ch));
+		curl_close($ch);
+		exit(1);
+	}
+
+	curl_close($ch);
+
+	$notice = json_decode($buffer);
+
+	if (!$notice->status->text) {
+		print "no notices yet.\n";
+		return;
+	}
+
+	print 'Current notice: ' . $notice->status->text . ' (' . $notice->status->created_at . ')' . "\n";
+	return $notice->status->created_at;
+
+}
+
+function process_item($item) {
+
+	global $current_notice_date, $HASHTAGS;
+
+	$new_notice_date = strtotime($item->get_date());
+
+	if ($new_notice_date > $current_notice_date) {
+		$title = truncate($item->get_title());
+		$link = ur1shorten($item->get_link());
+		$notice = "$title $link";
+		$noticelen = strlen($notice);
+
+		# Tack on some tags, if there's room
+		foreach ($HASHTAGS as $keyword => $tag) {
+			if (eregi($keyword, $notice)) {
+				if ( ($noticelen + strlen($tag) + 1) <= 140 ) {
+					$notice = $notice . " $tag";
+				}
+			}
+		}
+		return $notice;
+	}
+	return NULL;
+}
+
+function send_notice($notice) {
+
+	global $NICKNAME, $PASSWORD, $LACONICA_UPDATE_URL;
+
+	$ch = setup_curl();
+	curl_setopt($ch, CURLOPT_URL, $LACONICA_UPDATE_URL);
+	curl_setopt($ch, CURLOPT_POST, 1);
+	curl_setopt($ch, CURLOPT_POSTFIELDS, array('status' => $notice, 'source' => 'rssdent'));
+	curl_setopt($ch, CURLOPT_USERPWD, "$NICKNAME:$PASSWORD");
+
+	$buffer = curl_exec($ch);
+
+	if (!$buffer) {
+		printf("send_notice - cURL error: %s\n", curl_error($ch));
+		curl_close($ch);
+		return NULL;
+	}
+
+	curl_close($ch);
+
+	print "Dent: $notice\n";
+
+	sleep(1); # Just to be polite
+
+	return;
+}
+
+function ur1shorten($url) {
+
+	$ch = curl_init();
+	curl_setopt($ch, CURLOPT_USERAGENT, "ur1shorten");
+	curl_setopt($ch, CURLOPT_URL,"http://ur1.ca");
+	curl_setopt($ch, CURLOPT_POST, 1);
+	curl_setopt($ch, CURLOPT_POSTFIELDS, "longurl=$url");
+	curl_setopt($ch, CURLOPT_FAILONERROR, 1);
+	curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+	curl_setopt($ch, CURLOPT_TIMEOUT, 10);
+	$html = curl_exec($ch);
+
+	if (!$html) {
+		printf("url1shorten - cURL error: %s\n", curl_error($ch));
+		curl_close($ch);
+		return NULL;
+	}
+
+	curl_close($ch);
+
+	$dom = new DOMDocument();
+	@$dom->loadHTML($html);
+	$xpath = new DOMXPath($dom);
+	$hrefs = $xpath->evaluate("/html/body/p[@class='success']/a");
+
+	return $hrefs->item(0)->getAttribute('href');
+}
+
+function truncate($str) {
+
+	if (strlen($str) > 100) {
+		// truncate at 100 chars -- Hey, we have to leave some room for the link
+		$str = substr($str, 0, 100) . '...';
+	}
+
+	return trim(htmlspecialchars_decode($str));
+}
+
+?>
